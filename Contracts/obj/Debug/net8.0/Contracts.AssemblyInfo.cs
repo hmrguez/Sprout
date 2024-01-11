d173a88ba9ed3946386d7f7b14790a8b223166fc
hmrguez <zealot.algo@gmail.com>
2024-01-11 12:35:08 -0500

    Untrack Api/obj folder

--- Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac1aeeab87e31fafa21a10a26b26f5205de79a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
